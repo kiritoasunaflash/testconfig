9a95f4714762066e27b3142b2e41061b6baf51e1
kirito <1970201491@qq.com>
2024-05-02 15:43:26 +0800

    feat: 修改test

--- src/stores/userInfo.js
+++ src/stores/userInfo.js
@@ -2,7 +2,6 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { regist, login } from '@/Api/regist'
 import { setToken, getToken, removeToken } from '@/utils/cookie'
-<<<<<<< HEAD
 export const userState = defineStore('userInfo', () => {
   const tokenTest = ref(null)
   tokenTest.value = '550e8400-e29b-41d4-a716-446655440000'
@@ -25,18 +24,4 @@ export const userState = defineStore('userInfo', () => {
 
   console.log(getToken('token'), 23)
   return { tokenTest, getTimer, timer }
-=======
-const tokenTest = ref(null)
-export const userState = defineStore('userInfo', () => {
-  tokenTest.value = '550e8400-e29b-41d4-a716-446655440000'
-  setToken(tokenTest.value)
-  let timer = new Date()
-  console.log(timer)
-  if (timer < 0) {
-    removeToken('token')
-  }
-  console.log(getToken('token'))
-  console.log(111)
-  return { tokenTest }
->>>>>>> feat-noFeel-my
 })
