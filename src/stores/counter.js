7c170965f5eba52e4faf4df40b7d23bb98ba82a4
kirito <1970201491@qq.com>
2024-05-02 15:41:52 +0800

    feat: 修改store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,9 +3,27 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
+  const data = ref([1, 23, 13, 12, 41, 123, 123, 24, 453, 42, 31, 243, 1])
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
+  }
+  const quickSort = (arr) => {
+    if (arr.length === 1) {
+      return arr
+    }
+    let left = []
+    let right = []
+    let middleIndex = Math.floor(arr.length / 2)
+    let middle = arr.splice(middleIndex, 1)[0]
+    for (let key in arr) {
+      if (arr[key] < middle) {
+        left.push(arr[key])
+      } else {
+        right.push(arr[key])
+      }
+    }
+    return quickSort(left).concat([middle], quickSort(right))
   }
 
   return { count, doubleCount, increment }
